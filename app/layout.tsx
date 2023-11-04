cf72451676b70f4ce3b4ecf89dce3ac971ba4ce6
tomatooy <ouyangzongxin@gmail.com>
2023-11-03 23:32:56 -0400

    layoutfix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import '@/app/ui/global.css'
 import { inter } from '@/app/ui/fonts';
-import SideNav from './ui/dashboard/sidenav';
 
 export default function RootLayout({
   children,
@@ -8,11 +7,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-64">
-        <SideNav />
-      </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
-    </div>
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
   );
 }
